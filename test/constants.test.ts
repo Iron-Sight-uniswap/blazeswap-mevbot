f1b3b3d4ca1f25f95c6e66754ccb106204c08546
John <denjohn@qq.com>
2024-02-14 01:17:49 +0800

    init publish

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,7 +1,9 @@
-import { INIT_CODE_HASH } from './constants'
-
-import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+import { INIT_CODE_HASH } from '../src/constants'
+import fs from 'fs'
+import path from 'path'
 import { keccak256 } from '@ethersproject/solidity'
+
+const bytecode = fs.readFileSync(path.join(__dirname, './bytecode'), 'utf8')
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
